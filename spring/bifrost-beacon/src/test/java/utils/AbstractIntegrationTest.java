e9e47b1d088dc4df9ef7222ee7531ccea1e6dc17
Marcin Gwóźdź <marcin.gwozdz@testto.com>
2024-10-02 22:09:58 +0200

    feat(database): add Liquibase for database migrations
    Integrate Liquibase to manage database schema changes efficiently. Updated dependencies in pom.xml and created necessary configuration and changelog files. Modified test setup to use PostgreSQL container.

--- spring/bifrost-beacon/src/test/java/utils/AbstractIntegrationTest.java
+++ spring/bifrost-beacon/src/test/java/utils/AbstractIntegrationTest.java
@@ -4,13 +4,23 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.riglightwatch.bifrostbeacon.BifrostBeaconApplication;
 import io.restassured.RestAssured;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@ActiveProfiles("test")
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = BifrostBeaconApplication.class)
 public class AbstractIntegrationTest {
@@ -30,8 +40,22 @@ public class AbstractIntegrationTest {
         objectMapper.registerModule(new JavaTimeModule());
     }
 
+    protected static final PostgreSQLContainer<?> postgres;
+
+    static {
+        postgres = new PostgreSQLContainer<>("postgres")
+                .withDatabaseName("bifrostbeacon")
+                .withUsername("postgres")
+                .withPassword("password");
+        postgres.start();
+    }
+
     @DynamicPropertySource
     static void registerProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.create.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.create.jdbc-url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.create.username", postgres::getUsername);
+        registry.add("spring.datasource.create.password", postgres::getPassword);
     }
 
 }
