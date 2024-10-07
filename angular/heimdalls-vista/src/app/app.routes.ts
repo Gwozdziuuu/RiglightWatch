cbbd6ecae52cb66464b5ce846fc0b34d0a9d5475
Marcin Gwóźdź <marcin.gwozdz@testto.com>
2024-10-07 21:15:19 +0200

    feat(components): add incidents and monitoring with navbar
    Added new IncidentsComponent and MonitoringComponent along with a NavbarComponent. Removed AppComponent test file and replaced the placeholder content in AppComponent with a navigation bar.

--- angular/heimdalls-vista/src/app/app.routes.ts
+++ angular/heimdalls-vista/src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {MonitoringComponent} from "./monitoring/monitoring.component";
+import {IncidentsComponent} from "./incidents/incidents.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'monitors', component: MonitoringComponent },
+  { path: 'incidents', component: IncidentsComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ angular/heimdalls-vista/src/app/navbar/navbar.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MatToolbar, MatToolbarRow} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import {MatAnchor, MatIconButton} from "@angular/material/button";
+import {MatSidenavContainer} from "@angular/material/sidenav";
+import {MatNavList} from "@angular/material/list";
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [
+    RouterLink,
+    MatToolbar,
+    MatToolbarRow,
+    MatIcon,
+    MatIconButton,
+    MatSidenavContainer,
+    MatNavList,
+    MatAnchor
+  ],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
